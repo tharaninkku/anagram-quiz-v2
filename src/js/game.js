53c0c957e24608f71bcab3896963e4819e7a1104
tharaninkku <tharaninkku@users.noreply.github.com>
2026-09-02 02:38:19 +0700

    basic game play loop without decoration

---
+++ src/js/game.js
@@ -0,0 +1,58 @@
+const words = [
+	"apple",
+	"banana",
+	"orange"
+];
+
+const random = document.getElementById("randomButton");
+const result = document.getElementById("randomResult");
+const check = document.getElementById("resultText");
+const playerInput = document.getElementById("playerInput");
+const score = document.getElementById("scoreNumber");
+let randomWord;
+let currentScore = 0;
+
+// use for get a random word from an array 
+function getRandomWord(array) {
+	randomWord = words[Math.floor(Math.random() * words.length)];
+	return randomWord;
+}
+
+// use for shuffle a word
+function getShuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}  
+
+//this function use getRandomWord and getShuffle
+function getGenerateWord() {
+	const shuffledWord = getShuffle(getRandomWord(words).split(""));
+	result.textContent = shuffledWord.join("");	
+}
+
+random.addEventListener("click", () => {
+	getGenerateWord();
+});
+
+playerInput.addEventListener("keydown", (event) => {
+	if (event.key === "Enter") {
+		if (playerInput.value === randomWord) {
+		check.textContent = "Correct!";
+		currentScore ++;
+		score.textContent = currentScore;
+		playerInput.value = "";
+		getGenerateWord();	
+	} else {
+		check.textContent = "Wrong!! Your score is reset";
+		currentScore = 0;
+		score.textContent = currentScore;
+		}	
+	}
+
+});
+
+
+
